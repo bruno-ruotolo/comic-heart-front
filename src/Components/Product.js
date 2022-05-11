fe9ea29af306c7c892c5125d536001f81cb54d26
Allan Ornel <allanornel@gmail.com>
2022-05-11 15:44:33 -0300

    feat: handleButton

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import styled from "styled-components";
@@ -6,6 +7,8 @@ import styled from "styled-components";
 export default function Product() {
   const [product, setProduct] = useState({});
   const { id } = useParams();
+  const navigate = useNavigate();
+
   useEffect(() => {
     const URL = "http://localhost:5000";
     async function getProduct() {
@@ -19,6 +22,17 @@ export default function Product() {
     getProduct();
   }, []);
 
+  async function handleButton() {
+    const URL = "http://localhost:5000";
+    try {
+      const promise = await axios.put(`${URL}/product/${id}`);
+      //TODO encaminhar para a página listagem de produtos
+      navigate("/");
+    } catch (e) {
+      console.log("Houve um problema ao adicionar o item ao carrinho" + e);
+    }
+  }
+
   return (
     <ProductSection>
       <h1>{product.name}</h1>
@@ -26,7 +40,13 @@ export default function Product() {
       <h2>{product.value}</h2>
       <p>{product.description}</p>
 
-      <button onClick={() => {}}>Adicionar ao Carrinho</button>
+      <button
+        onClick={() => {
+          handleButton();
+        }}
+      >
+        Adicionar ao Carrinho
+      </button>
     </ProductSection>
   );
 }
