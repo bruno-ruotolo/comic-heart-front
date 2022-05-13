feffaadc7cfe81873fb175d2933b5774d76609f6
allanornel <allanornel@gmail.com>
2022-05-13 01:42:04 -0300

    feat: start of checkoutpage

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -5,9 +5,23 @@ import styled from "styled-components";
 import Swal from "sweetalert2";
 
 import { UserContext } from "../Context/UserContext ";
-import Header from "./Header/Header";
 
 export default function Checkout() {
   const { userInfos } = useContext(UserContext);
   const navigate = useNavigate();
+
+  return (
+    <CheckoutSection>
+      <h1>Selecione a forma de pagamento</h1>
+      <form>
+        <input></input> Boleto Vencimento em 1 dia útil
+        <input></input> Pix Vencimento em 30 minutos
+        <input></input> Cartão de Crédito Cliente: User E-mail: User e-mail
+        Digite seu CPF: <input type="text" placeholder="CPF"></input>
+        <button type="submit">Confirmar (R$ valorTotal)</button>
+      </form>
+    </CheckoutSection>
+  );
 }
+
+const CheckoutSection = styled.section``;
